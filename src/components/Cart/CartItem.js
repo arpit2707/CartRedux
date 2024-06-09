0a83dbe2ea2da3a48709e5b859990a5dde021795
arpit2707 <arpitgaurav.goldi723@gmail.com>
2024-06-09 22:33:13 +0530

    cartItems

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,15 @@
-import classes from './CartItem.module.css';
-
+import classes from "./CartItem.module.css";
+import { useDispatch } from "react-redux";
+import { cartSliceActions } from "../../store/cart-slice";
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
-
+  const { id, title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +18,36 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button
+            onClick={() => {
+              dispatch(
+                cartSliceActions.removeFromCart({
+                  id: id,
+                  title: title,
+                  quantity: quantity,
+                  total: total,
+                  price: price,
+                })
+              );
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={() => {
+              dispatch(
+                cartSliceActions.addToCart({
+                  id: id,
+                  title: title,
+                  quantity: quantity,
+                  total: total,
+                  price: price,
+                })
+              );
+            }}
+          >
+            +
+          </button>
         </div>
       </div>
     </li>
