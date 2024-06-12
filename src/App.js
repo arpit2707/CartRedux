0f47861e46be7ce59a3caaaf76b7f35214be9770
arpit2707 <arpitgaurav.goldi723@gmail.com>
2024-06-12 19:52:14 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
+import { Fragment, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 function App() {
+  const dispatch = useDispatch();
   const cartToggle = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const updateCart = async () => {
+    dispatch(
+      uiActions.showNotification({
+        status: "Pending",
+        title: "Sending",
+        message: "Sending Cart Data",
+      })
+    );
+    const response = await fetch(
+      "https://indigo-pod-388318-default-rtdb.firebaseio.com/expenses.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+
+    if (!response.ok) {
+      uiActions.showNotification({
+        status: "Failed",
+        title: "Failure!...",
+        message: "Sending Cart Data Failed",
+      });
+      throw new Error("Sending cart data failed");
+    }
+
+    uiActions.showNotification({
+      status: "Success",
+      title: "Success!...",
+      message: "Sent Cart Data Successful",
+    });
+  };
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    updateCart();
+  }, [cart]);
   return (
-    <Layout>
-      {cartToggle && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartToggle && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,13 +4,12 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
-    totalQuantity: 1,
+    totalQuantity: 0,
     totalAmount: 0,
   },
   reducers: {
     addToCart(state, action) {
       const newItem = action.payload;
-
       const existingItem = state.items.find((iteem) => iteem.id == newItem.id);
       console.log("NewItem 13", newItem, state.items, action);
       if (!existingItem) {
@@ -54,3 +53,31 @@ const cartSlice = createSlice({
 });
 export const cartSliceActions = cartSlice.actions;
 export default cartSlice;
+
+// Note:- Here the learning is we cant make any side effect work inside our reducers and making
+//an api call is one of that because our reducers should be pure function , means it should
+// return same output on same input and that will easy for testing too. Reducers are meant to be predictable , making api calls
+// introduces asynchronous behaviour which can lead to unpredictable state changes. Reducers
+// are time consuming making call inside reducers can make delay of state changes. Since reducers
+// call frequently during state updates , making api call within them can lead to inconsistent state results
+
+// Now we have two options to make that call 1.) Inside the components (e.g useEffect()) ,
+// 2.) inside the action creators.
+
+//We should not update state when using useselector because useselector responsibility to only
+// read the state , 2. redux follows the unidirectional data flow. It is necessary to maintain
+// predcitability of changes and maintainability of it.
+
+// we also have option to get hold on state using useSelector make a copy of that object then
+// without mutating the state we can make changes to copy of state and then define a reducer which will
+// update this copy of state with the pre-existing state
+
+// We would always have these option where to keep our major changes inside , 1.) inside fat components
+// 2.) fat reducers , 3.) fat actions
+
+// Synchronous and side-effect free code should go inside reducers instead of action creators or components.
+// Async code or code with side effects prefer action creators or components , never use reducers
+
+//Thunk is a function that delays an action until later. An action creator function that does not
+// return the action but a function which will return the action. THis is action {type:"" , payload:""}
+//which redux toolkit in action creators automatically creates for us.
